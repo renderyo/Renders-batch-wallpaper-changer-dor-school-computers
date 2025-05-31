1c5ac281d9e9b2553df4455b6eea398cb41e0206
renderyo <lohrhebert@gmail.com>
2025-05-31 17:43:20 -0300

    Update watchdog3.bat
    v1

--- watchdog3.bat
+++ watchdog3.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal
+
 set SCRIPT_PATH=%~dp0chrome_monitor.ps1
 
 :: Add to startup
@@ -8,5 +10,12 @@ copy "%SCRIPT_PATH%" "%STARTUP_FOLDER%\chrome_monitor.ps1" >nul
 
 :: Run PowerShell script in background
 powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%SCRIPT_PATH%"
+
+:: Disable changing wallpaper via Group Policy
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoChangingWallPaper /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoComponents /t REG_DWORD /d 1 /f
+
+:: Optional: Force a group policy update
+gpupdate /force
 
 exit
